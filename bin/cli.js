e438be9f897704931c69093184acd9e98f2a3c6e
Sylvain <hello@sylvainsimao.fr>
2021-04-10 17:20:42 +1000

    add option to override the generator

--- bin/cli.js
+++ bin/cli.js
@@ -6,13 +6,19 @@ cli
     .command('<target-folder>', 'Scaffold boilerplate to target folder')
     .option(
         '--pm <package-manager>',
-        `Choose an package manager ('yarn' | 'npm')`
+        `Choose an package manager ('yarn' | 'npm')`,
+        { default: 'yarn' }
     )
-    .action(async (targetFolder, { pm }) => {
+    .option(
+        '--generator <generator>',
+        `Choose a generator ('maoosi/prisma-appsync#next' | './my-local-prisma-appsync')`,
+        { default: 'maoosi/prisma-appsync#main' }
+    )
+    .action(async (targetFolder, { pm, generator }) => {
         const sao = require('sao')
 
         const app = sao({
-            generator: 'maoosi/prisma-appsync#main',
+            generator: generator,
             outDir: targetFolder,
             npmClient: pm
         })
